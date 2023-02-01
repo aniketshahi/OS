3881f4340fc6267c8c99d27998f434ba6157a0b5
aniket0608 <aniketshahi7766@gmail.com>
2023-02-01 19:12:17 +0530

    modified:   Compements.cpp 	modified:   Compements.exe

--- Compements.cpp
+++ Compements.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
-#include <cmath>
+#include <string>
+
 using namespace std;
-int onesComplement(int n)
-{
-    int bitcount = floor(log2(n)) + 1;
-    int ones = ((1 << bitcount) - 1);
-    return ones ^ n;
+
+void onesComplement(string binary, string &result) {
+    int length = binary.length();
+    for (int i = 0; i < length; i++) {
+        if (binary[i] == '0')
+            result[i] = '1';
+        else
+            result[i] = '0';
+    }
 }
-int main()
-{ 
-    int a;
-    cout << "Enter number:";
-    cin >> a;
-    cout << "One's Complement of " << a << " is: " << onesComplement(a);
+
+void twosComplement(string binary, string &result) {
+    int length = binary.length();
+    onesComplement(binary, result);
+    int carry = 1;
+    for (int i = length - 1; i >= 0; i--) {
+        if (result[i] == '0' && carry == 1) {
+            result[i] = '1';
+            carry = 0;
+        } else if (result[i] == '1' && carry == 1) {
+            result[i] = '0';
+            carry = 1;
+        }
+    }
+}
+
+int main() {
+    string binary, onesComp, twosComp;
+    cout << "Enter a binary number: ";
+    cin >> binary;
+    onesComp = binary;
+    twosComp = binary;
+    onesComplement(binary, onesComp);
+    twosComplement(binary, twosComp);
+    cout << "1's complement: " << onesComp << endl;
+    cout << "2's complement: " << twosComp << endl;
+    return 0;
 }
