0648799be94c2daafb27e19b61e217374debeb75
aniket0608 <aniketshahi7766@gmail.com>
2023-03-25 12:57:11 +0530

    modified:   ganttchart.cpp

--- ganttchart.cpp
+++ ganttchart.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 
 using namespace std;
 
 // Define a struct to hold the process details
-struct Process {
+struct Process
+{
     string name;
     int start_time;
     int end_time;
+    int completion_time;
 };
 
-int main() {
+int main()
+{
     // Define the processes
     vector<Process> processes = {
         {"Process A", 0, 4},
@@ -21,46 +25,55 @@ int main() {
         {"Process E", 5, 7},
     };
 
-    // Calculate the duration of each process and determine the maximum duration
-    int max_duration = 0;
-    for (const auto& process : processes) {
-        int duration = process.end_time - process.start_time;
-        if (duration > max_duration) {
-            max_duration = duration;
+    // Calculate the duration and completion time of each process and determine the maximum completion time
+    int max_completion_time = 0;
+    for (auto &process : processes)
+    {
+        process.completion_time = process.end_time;
+        for (const auto &other_process : processes)
+        {
+            if (other_process.start_time >= process.end_time && other_process.completion_time < process.completion_time)
+            {
+                process.completion_time = other_process.completion_time;
+            }
+        }
+        if (process.completion_time > max_completion_time)
+        {
+            max_completion_time = process.completion_time;
         }
     }
 
-    // Determine the number of characters needed to represent the maximum duration
-    int duration_width = to_string(max_duration).size();
+    // Determine the number of characters needed to represent the maximum completion time
+    int completion_time_width = to_string(max_completion_time).size();
 
     // Print the chart header
     cout << "                    Gantt Chart" << endl;
-    cout << "---------------------------------------------------" << endl;
+    cout << "---------------------------------------------------------------" << endl;
 
     // Print the processes
-    for (const auto& process : processes) {
+    for (const auto &process : processes)
+    {
         // Calculate the duration and the number of spaces needed for alignment
         int duration = process.end_time - process.start_time;
         string spaces;
-        for (int i = 0; i < process.start_time * (duration_width + 1); i++) {
+        spaces.reserve(process.start_time * (completion_time_width + 1));
+        for (int i = 0; i < process.start_time * (completion_time_width + 1); i++)
+        {
             spaces += " ";
         }
 
         // Print the process name and the necessary number of spaces
-        cout << "| " << process.name << spaces;
+        cout << "| " << setw(process.name.size() + process.start_time * (completion_time_width + 1)) << process.name << " ";
 
         // Print the horizontal bar
         cout << string(duration, '-') << " ";
 
-        // Print the remaining spaces
-        for (int i = 0; i < (max_duration - duration) * (duration_width + 1); i++) {
-            cout << " ";
-        }
-        cout << "|" << endl;
+        // Print the completion time
+        cout << setw(completion_time_width) << process.completion_time << "|" << endl;
     }
 
     // Print the chart footer
-    cout << "---------------------------------------------------" << endl;
+    cout << "---------------------------------------------------------------" << endl;
 
     return 0;
 }
