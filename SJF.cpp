c84e244b2cf25bb4bf82fde195bacac7c8499daf
aniket0608 <aniketshahi7766@gmail.com>
2023-03-26 11:57:56 +0530

    new file:   SJF.exe

--- SJF.cpp
+++ SJF.cpp
@@ -45,91 +45,61 @@ int main()
     // Initialize the first process
     completion_times[0] = processes[0].arrival_time + processes[0].burst_time;
     waiting_times[0] = 0;
-    turnaround_times[0] = processes[0].burst_time;
+    turnaround_times[0] = completion_times[0] - processes[0].arrival_time;
     response_times[0] = processes[0].burst_time;
 
-    // Calculate completion, waiting, turnaround, and response times for the rest of the processes
+    // Calculate completion, waiting, and turnaround times for each process
     for (int i = 1; i < n; i++)
     {
-        // Sort processes based on burst time among those that have arrived
-        sort(processes.begin() + i, processes.end(), compare_burst_time);
+        int previous_completion_time = completion_times[i - 1];
+        int current_arrival_time = processes[i].arrival_time;
+        int current_burst_time = processes[i].burst_time;
 
-        int j = i - 1;
-
-        // Calculate completion time of the current process
-        while (j >= 0 && processes[j].arrival_time + processes[j].burst_time > processes[i].arrival_time)
+        // If the process arrives after the previous process completes, its completion time is its arrival time plus its burst time
+        if (current_arrival_time > previous_completion_time)
         {
-            j--;
+            completion_times[i] = current_arrival_time + current_burst_time;
         }
-        completion_times[i] = max(processes[i].arrival_time, completion_times[j] + processes[j].burst_time) + processes[i].burst_time;
+        // Otherwise, its completion time is the previous completion time plus its burst time
+        else
+        {
+            completion_times[i] = previous_completion_time + current_burst_time;
+        }
 
-        // Calculate waiting time, turnaround time, and response time of the current process
-        waiting_times[i] = completion_times[i] - processes[i].arrival_time - processes[i].burst_time;
+        // Calculate waiting time, turnaround time, and response time for the process
+        waiting_times[i] = completion_times[i - 1] - processes[i].arrival_time;
         turnaround_times[i] = completion_times[i] - processes[i].arrival_time;
-        response_times[i] = completion_times[i - 1] < processes[i].arrival_time ? processes[i].burst_time : completion_times[i - 1] - processes[i].arrival_time;
-
+        response_times[i] = waiting_times[i] + processes[i].burst_time;
     }
 
-    // Calculate average waiting time, turnaround time, and response time
-    double avg_waiting_time = 0;
-    double avg_turnaround_time = 0;
-    double avg_response_time = 0;
+    // Calculate the average waiting time, turnaround time, and response time
+    double total_waiting_time = 0;
+    double total_turnaround_time = 0;
+    double total_response_time = 0;
+
     for (int i = 0; i < n; i++)
     {
-        avg_waiting_time += waiting_times[i];
-        avg_turnaround_time += turnaround_times[i];
-        avg_response_time += response_times[i];
+        total_waiting_time += waiting_times[i];
+        total_turnaround_time += turnaround_times[i];
+        total_response_time += response_times[i];
     }
-    avg_waiting_time /= n;
-    avg_turnaround_time /= n;
-    avg_response_time /= n;
 
-    // Calculate CPU utilization
-    int total_cpu_time = 0;
+    double average_waiting_time = total_waiting_time / n;
+    double average_turnaround_time = total_turnaround_time / n;
+    double average_response_time = total_response_time / n;
+
+    // Print the completion, waiting, and turnaround times for each process
+    cout << "Process\tCompletion Time\tWaiting Time\tTurnaround Time\tResponse Time\n";
+
     for (int i = 0; i < n; i++)
     {
-        total_cpu_time += processes[i].burst_time;
+        cout << processes[i].id << "\t\t" << completion_times[i] << "\t\t" << waiting_times[i] << "\t\t" << turnaround_times[i] << "\t\t" << response_times[i] << "\n";
     }
-    double cpu_utilization = (double)total_cpu_time / completion_times[n - 1];
 
-    // Print the Gantt chart
-cout << "Gantt chart:" << endl;
-int current_time = processes[0].arrival_time;
-for (int i = 0; i < n; i++)
-{
-    cout << "| P" << processes[i].id << " ";
-    current_time = completion_times[i];
-}
-cout << "|" << endl;
-cout << "0";
-for (int i = 0; i < n; i++)
-{
-    cout << "      " << completion_times[i];
-}
-cout << endl;
-// Calculate CPU utilization
-int total_burst_time = 0;
-for (int i = 0; i < n; i++)
-{
-    total_burst_time += processes[i].burst_time;
-}
-double cpu_utilization = (double)total_burst_time / (double)completion_times[n - 1];
+    // Print the average waiting time, turnaround time, and response time
+    cout << "Average waiting time: " << average_waiting_time << "\n";
+    cout << "Average turnaround time: " << average_turnaround_time << "\n";
+    cout << "Average response time: " << average_response_time << "\n";
 
-// Print CPU utilization
-cout << endl << "CPU utilization: " << cpu_utilization * 100 << "%" << endl;
-// Print the completion, waiting, turnaround, and response times of each process
-cout << endl;
-cout << "Process\t\tCompletion Time\tWaiting Time\tTurnaround Time\tResponse Time" << endl;
-for (int i = 0; i < n; i++)
-{
-    cout << "P" << processes[i].id << "\t\t" << completion_times[i] << "\t\t" << waiting_times[i] << "\t\t" << turnaround_times[i] << "\t\t" << response_times[i] << endl;
+    return 0;
 }
-
-// Print the average waiting time, turnaround time, response time, and CPU utilization
-cout << endl;
-cout << "Average waiting time: " << avg_waiting_time << endl;
-cout << "Average turnaround time: " << avg_turnaround_time << endl;
-cout << "Average response time: " << avg_response_time << endl;
-cout << "CPU utilization: " << cpu_utilization << endl;
-
-return 0;
