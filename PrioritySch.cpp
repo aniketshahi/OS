572d933ba3ccb671add50388e06b2513e5e25c8d
aniket0608 <aniketshahi7766@gmail.com>
2023-04-04 21:32:17 +0530

    modified:   PrioritySch.exe

--- PrioritySch.cpp
+++ PrioritySch.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -24,17 +25,25 @@ int main() {
     int n;
     std::cout << "Enter the number of processes: ";
     std::cin >> n;
-    
+
     std::vector<Process> processes(n);
     for (int i = 0; i < n; ++i) {
         processes[i].id = i + 1;
         std::cout << "Enter arrival time, burst time, and priority for process " << i + 1 << ": ";
         std::cin >> processes[i].arrival_time >> processes[i].burst_time >> processes[i].priority;
     }
-    
+
     std::sort(processes.begin(), processes.end(), compareArrivalTime);
     int current_time = processes[0].arrival_time;
-    
+
+    int total_turnaround_time = 0;
+    int total_waiting_time = 0;
+    int total_burst_time = 0;
+
+    std::cout << std::left << std::setw(5) << "P#" << std::setw(15) << "Arrival Time" << std::setw(15)
+              << "Burst Time" << std::setw(15) << "Priority" << std::setw(15) << "Completion Time"
+              << std::setw(15) << "Turnaround Time" << "Waiting Time" << std::endl;
+
     while (!processes.empty()) {
         std::vector<Process> ready_queue;
         for (const auto &p : processes) {
@@ -53,17 +62,28 @@ int main() {
             selected_process.waiting_time = selected_process.turnaround_time - selected_process.burst_time;
             current_time = selected_process.completion_time;
 
+            total_turnaround_time += selected_process.turnaround_time;
+            total_waiting_time += selected_process.waiting_time;
+            total_burst_time += selected_process.burst_time;
+
             processes.erase(std::remove_if(processes.begin(), processes.end(),
                                            [&](const Process &p) { return p.id == selected_process.id; }),
                             processes.end());
-            std::cout << "P" << selected_process.id << "\t" << selected_process.arrival_time << "\t"
-                      << selected_process.burst_time << "\t" << selected_process.priority << "\t"
-                      << selected_process.completion_time << "\t" << selected_process.turnaround_time << "\t"
-                      << selected_process.waiting_time << std::endl;
+            std::cout << "P" << std::left << std::setw(4) << selected_process.id << std::setw(15)
+                      << selected_process.arrival_time << std::setw(15) << selected_process.burst_time << std::setw(15)
+                      << selected_process.priority << std::setw(15) << selected_process.completion_time
+                      << std::setw(15) << selected_process.turnaround_time << selected_process.waiting_time
+                      << std::endl;
         } else {
             current_time++;
         }
     }
+
+    double cpu_utilization = (static_cast<double>(total_burst_time) / current_time) * 100.0;
+    double throughput = static_cast<double>(n) / total_turnaround_time;
+
+    std::cout << "CPU Utilization: " << cpu_utilization << "%" << std::endl;
+    std::cout << "Throughput: " << throughput << " processes/unit time" << std::endl;
 
     return 0;
 }
