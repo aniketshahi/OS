014575e8503144e55ea2402d9a060fba9b183f68
aniket0608 <aniketshahi7766@gmail.com>
2023-05-07 21:15:41 +0530

    modified:   SJN.cpp

--- SJN.cpp
+++ SJN.cpp
@@ -1,107 +1,79 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-
-using namespace std;
-
-struct Process {
-    int pid;
-    int arrival_time;
-    int burst_time;
-    int completion_time;
-    int turn_around_time;
-    int waiting_time;
-};
-
-bool compareArrivalTime(const Process& a, const Process& b) {
-    return a.arrival_time < b.arrival_time;
-}
-
-bool compareBurstTime(const Process& a, const Process& b) {
-    return a.burst_time < b.burst_time;
-}
-
-void calculateCompletionTime(vector<Process>& processes) {
-    int n = processes.size();
-    int current_time = 0;
-    for (int i = 0; i < n; i++) {
-        if (current_time < processes[i].arrival_time) {
-            current_time = processes[i].arrival_time;
-        }
-        processes[i].completion_time = current_time + processes[i].burst_time;
-        current_time = processes[i].completion_time;
-    }
-}
-
-void calculateTurnAroundTime(vector<Process>& processes) {
-    int n = processes.size();
-    for (int i = 0; i < n; i++) {
-        processes[i].turn_around_time = processes[i].completion_time - processes[i].arrival_time;
-    }
-}
-
-void calculateWaitingTime(vector<Process>& processes) {
-    int n = processes.size();
-    for (int i = 0; i < n; i++) {
-        processes[i].waiting_time = processes[i].turn_around_time - processes[i].burst_time;
-    }
-}
-
-void printTable(const vector<Process>& processes) {
-    int n = processes.size();
-    cout << "PID\tArrival Time\tBurst Time\tCompletion Time\tTurn Around Time\tWaiting Time\n";
-    for (int i = 0; i < n; i++) {
-        cout << processes[i].pid << "\t" << processes[i].arrival_time << "\t\t" << processes[i].burst_time << "\t\t"
-            << processes[i].completion_time << "\t\t" << processes[i].turn_around_time << "\t\t\t"
-            << processes[i].waiting_time << endl;
-    }
-}
-
-int main() {
-    int n;
-    cout << "Enter the number of processes: ";
-    cin >> n;
-
-    vector<Process> processes(n);
-
-    for (int i = 0; i < n; i++) {
-        cout << "Enter arrival time and burst time for process " << i + 1 << ": ";
-        cin >> processes[i].arrival_time >> processes[i].burst_time;
-        processes[i].pid = i + 1;
-    }
-
-    sort(processes.begin(), processes.end(), compareArrivalTime);
-
-    vector<Process> ready_queue;
-    int current_time = 0;
-    int completed_processes = 0;
-
-    while (completed_processes < n) {
-        for (int i = 0; i < n; i++) {
-            if (processes[i].arrival_time <= current_time && processes[i].completion_time == 0) {
-                ready_queue.push_back(processes[i]);
-            }
-        }
-
-        if (!ready_queue.empty()) {
-            sort(ready_queue.begin(), ready_queue.end(), compareBurstTime);
-            Process next_process = ready_queue.front();
-            ready_queue.erase(ready_queue.begin());
-
-            next_process.completion_time = current_time + next_process.burst_time;
-            current_time = next_process.completion_time;
-
-            completed_processes++;
-            processes[next_process.pid - 1] = next_process;
-        } else {
-            current_time++;
-        }
-    }
-
-    calculateCompletionTime(processes);
-    calculateTurnAroundTime(processes);
-    calculateWaitingTime(processes);
-    printTable(processes);
-
-    return 0;
-}
+#include <bits/stdc++.h>  
+using namespace std;  
+//structure for every process  
+struct Process {  
+   int pid; // Process ID  
+   int bt; // Burst Time  
+   int art; // Arrival Time  
+};  
+void findTurnAroundTime(Process proc[], int n, int wt[], int tat[]) {  
+   for (int i = 0; i < n; i++)  
+   tat[i] = proc[i].bt + wt[i];  
+}  
+//waiting time of all process  
+void findWaitingTime(Process proc[], int n, int wt[]) {  
+   int rt[n];  
+   for (int i = 0; i < n; i++)  
+   rt[i] = proc[i].bt;  
+   int complete = 0, t = 0, minm = INT_MAX;  
+   int shortest = 0, finish_time;  
+   bool check = false;  
+   while (complete != n) {  
+      for (int j = 0; j < n; j++) {  
+         if ((proc[j].art <= t) && (rt[j] < minm) && rt[j] > 0) {  
+            minm = rt[j];  
+            shortest = j;  
+            check = true;  
+         }  
+      }  
+      if (check == false) {  
+         t++;  
+         continue;  
+      }  
+      // decrementing the remaining time  
+      rt[shortest]--;  
+      minm = rt[shortest];  
+      if (minm == 0)  
+         minm = INT_MAX;  
+         // If a process gets completely  
+         // executed  
+         if (rt[shortest] == 0) {  
+            complete++;  
+            check = false;  
+            finish_time = t + 1;  
+            // Calculate waiting time  
+            wt[shortest] = finish_time -  
+            proc[shortest].bt -  
+            proc[shortest].art;  
+            if (wt[shortest] < 0)  
+               wt[shortest] = 0;  
+         }  
+         // Increment time  
+         t++;  
+   }  
+}  
+// Function to calculate average time  
+void findavgTime(Process proc[], int n) {  
+   int wt[n], tat[n], total_wt = 0,  
+   total_tat = 0;  
+   // Function to find waiting time of all  
+   // processes  
+   findWaitingTime(proc, n, wt);  
+   // Function to find turn around time for  
+   // all processes  
+   findTurnAroundTime(proc, n, wt, tat);  
+   cout << "Processes " << " Burst time " << " Waiting time " << " Turn around time\n";  
+   for (int i = 0; i < n; i++) {  
+      total_wt = total_wt + wt[i];  
+      total_tat = total_tat + tat[i];  
+      cout << " " << proc[i].pid << "\t\t" << proc[i].bt << "\t\t " << wt[i] << "\t\t " << tat[i] << endl;  
+   }  
+   cout << "\nAverage waiting time = " << (float)total_wt / (float)n; cout << "\nAverage turn around time = " << (float)total_tat / (float)n;  
+}  
+// main function  
+int main() {  
+   Process proc[] = { { 1, 5, 1 }, { 2, 3, 1 }, { 3, 6, 2 }, { 4, 5, 3 } };  
+   int n = sizeof(proc) / sizeof(proc[0]);  
+   findavgTime(proc, n);  
+   return 0;  
+}  
