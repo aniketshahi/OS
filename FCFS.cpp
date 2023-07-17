02579786708af8dbaf607e59f021da9e35665795
aniket0608 <aniketshahi7766@gmail.com>
2023-07-17 08:31:50 +0530

    modified:   FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
-class Process
+class Process 
 {
 public:
     int Pid;
@@ -16,74 +15,76 @@ public:
     int THPT;
     int CPU;
 
+    Process() : Pid(0), AT(0), BT(0), CT(0), WT(0), TAT(0), THPT(0), CPU(0)
+    {
+    }
+
     Process(int pid, int at, int bt)
         : Pid(pid), AT(at), BT(bt), CT(0), WT(0), TAT(0), THPT(0), CPU(0)
     {
     }
 };
 
-bool compare_AT(const Process &p1, const Process &p2)
+bool compare_AT(const Process& p1, const Process& p2) 
 {
     return p1.AT < p2.AT;
 }
 
-void print_table(const vector<Process> &processes, float avg_WT, float avg_TAT, float THPT)
+void print_table(Process* processes, int n, float avg_WT, float avg_TAT, float THPT) 
 {
     cout << "Pid\tAT\tBT\tCT\tTAT\tWT " << endl;
-    for (const Process &process : processes)
-    {
-        cout << process.Pid << "\t" << process.AT << "\t" << process.BT << "\t" << process.CT << "\t" << process.TAT << "\t" << process.WT << endl;
+    for (int i = 0; i < n; i++) {
+        cout << processes[i].Pid << "\t" << processes[i].AT << "\t" << processes[i].BT << "\t" << processes[i].CT << "\t" << processes[i].TAT << "\t" << processes[i].WT << endl;
     }
     cout << "Average WT is: " << avg_WT << endl;
     cout << "Average TAT is: " << avg_TAT << endl;
     cout << "Throughput is: " << THPT << endl;
 }
 
-void fcfs_scheduling(vector<Process> &processes)
+void fcfs_scheduling(Process* processes, int n) 
 {
     int current_time = 0;
     float total_WT = 0, total_TAT = 0;
-    for (Process &process : processes)
-    {
-        if (current_time < process.AT)
-        {
-            current_time = process.AT;
+    for (int i = 0; i < n; i++) {
+        if (current_time < processes[i].AT) {
+            current_time = processes[i].AT;
         }
-        current_time += process.BT;
-        process.CT = current_time;
-        process.TAT = process.CT - process.AT;
-        process.WT = process.TAT - process.BT;
-        total_WT += process.WT;
-        total_TAT += process.TAT;
+        current_time += processes[i].BT;
+        processes[i].CT = current_time;
+        processes[i].TAT = processes[i].CT - processes[i].AT;
+        processes[i].WT = processes[i].TAT - processes[i].BT;
+        total_WT += processes[i].WT;
+        total_TAT += processes[i].TAT;
     }
-    float avg_WT = total_WT / processes.size();
-    float avg_TAT = total_TAT / processes.size();
-    float THPT = static_cast<float>(processes.size()) / current_time;
+    float avg_WT = total_WT / n;
+    float avg_TAT = total_TAT / n;
+    float THPT = static_cast<float>(n) / current_time;
 
-    print_table(processes, avg_WT, avg_TAT, THPT);
+    print_table(processes, n, avg_WT, avg_TAT, THPT);
 }
 
-int main()
+int main() 
 {
     int n;
     cout << "Enter the number of processes: ";
     cin >> n;
 
-    vector<Process> processes;
-    for (int i = 0; i < n; i++)
-    {
+    Process* processes = new Process[n];
+    for (int i = 0; i < n; i++) {
         int pid, at, bt;
         pid = i + 1;
         cout << "Enter the AT of process " << pid << ": ";
         cin >> at;
         cout << "Enter the BT of process " << pid << ": ";
         cin >> bt;
-        processes.push_back(Process(pid, at, bt));
+        processes[i] = Process(pid, at, bt);
     }
 
-    sort(processes.begin(), processes.end(), compare_AT);
+    sort(processes, processes + n, compare_AT);
 
-    fcfs_scheduling(processes);
+    fcfs_scheduling(processes, n);
+
+    delete[] processes;
 
     return 0;
 }
